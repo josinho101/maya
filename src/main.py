8c0bd07fa708c386a58a1a14ea3775678fba5a2f
Joseph <josinho.seven@gmail.com>
2026-06-23 07:50:01 +0530

    Updated the plan for the implementation

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from lib.face_recognizer import FaceRecognizer
-
-model_csv = 'src/content/model.csv'
-image_base_path = 'src/content/images'
-
-fr = FaceRecognizer(model_csv, image_base_path)
-fr.train_models()
-
-print('model training completed')
